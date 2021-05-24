fa988fa4933662307c19cfb3d223b5837b475b11
3Strela <mateusestrelapietro@gmail.com>
2021-05-24 20:06:05 -0300

    Update 2013

---
+++ ACM-ICPC Brazil Subregional/AnyEx/Coral.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    double n;
+    while (scanf("%lf", &n) != EOF)
+    {
+        int sm = 0, v[10014], i;
+        for (i = 0; i < n; i++)
+        {
+            scanf("%d", &v[i]);
+            sm += v[i];
+        }
+
+        int md = sm / n;
+        double sla = sm / n;
+        if (md != sla)
+            printf("-1\n");
+        else
+        {
+            int ans = 1;
+            for (i = 0; i < n; i++)
+                if (v[i] < md)
+                    ans += md - v[i];
+
+            printf("%d\n", ans);
+        }
+    }
+
+    return 0;
+}
